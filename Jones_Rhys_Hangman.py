2049315a8499062ce3f28dbf9e5e3a98dd4d915e
jonesr364 <91385528+jonesr364@users.noreply.github.com>
2022-04-14 09:19:11 +0100

    Update Jones_Rhys_Hangman.py

--- Jones_Rhys_Hangman.py
+++ Jones_Rhys_Hangman.py
@@ -1,7 +1,10 @@
+# (JCS) Module 3: python
+# Completed by Rhys Jones
 
 import random
 from collections import Counter
 
+# import word list document
 with open("word_list.txt") as f:
         word_list = f.read().splitlines()
 
@@ -9,27 +12,32 @@ word = random.choice(word_list)
 
 if __name__ == '__main__':
         print('Please enter your next guess: ')
-	
+
+	#Print spaces for letters in word
         for i in word:
                 print('*', end = ' ')		
         print()
 
         playing = True
+        #store letter guesses found in word
         guesses = ''
+        # store all the guessed letters
         totalguesses = ''
         lives = 7
         correct = 0
         flag = 0
         try:
-                while (lives > 0) and flag == 0:
+                while (lives > 0) and flag == 0: #flag updates when word is completed
                         print()
 
                         try:
-                                guess = str(input('Please enter your next guess: '))
+                                userguess = str(input('Please enter your next guess: '))
+                                guess = userguess.lower()
                         except:
                                 print('Only enter letters')
                                 continue
 
+                        #validate the character entered
                         if not guess.isalpha():
                                 print('Only enter letter characters')
                                 continue
@@ -40,33 +48,41 @@ if __name__ == '__main__':
                                 print('You have guessed that already')
                                 continue
 
+                        # if the letter guessed is in the word
                         if guess in word:
-                                k = word.count(guess) 
+                                k = word.count(guess) # k stores the number of times the letter appears in the word
                                 for _ in range(k):	
-                                        totalguesses += guess
-                                        guesses += guess
+                                        totalguesses += guess #Add the letter to the bank of guessed letters
+                                        guesses += guess #The guessed letter is added to the word
+                        # if the letter is guessed is not in the word
                         else:
-                                lives -=1
-                                totalguesses +=guess
+                                lives -=1 # Remove a life for incorrectly guessing
+                                totalguesses +=guess # Add the letter to the bank of guessed letters
+                                print('Incorrect Guess. Lives left: ', end=' ')
+                                print(lives)
 
+                        # Print the word
                         for char in word:
                                 if char in guesses and (Counter(guesses) != Counter(word)):
                                         print(char, end = ' ')
                                         correct += 1
+                                #If all letters have been guessed
                                 elif (Counter(guesses) == Counter(word)): 																# the game ends, even if chances remain
-                                        print("The word is: ", end=' ')
+                                        print('The word is: ', end=' ')
                                         print(word)
                                         flag = 1
                                         print('Congratulations you win')
-                                        break 
-                                        break 
+                                        break # Break for loop
+                                        break # break while loop
                                 else:
                                         print('*', end = ' ')
 			
-
+                # When the user runs of of lives
                 if lives <= 0 and (Counter(guesses) != Counter(word)):
                         print()
                         print('You lose')
+                        print("The word was: ", end=' ')
+                        print(word)
 
         except KeyboardInterrupt:
                 print()
